af5793dd7e315de579ef0d21a49e8d6bf37e3333
HernanD <hdavella@gmail.com>
2025-08-28 19:30:18 -0300

    aspect import

---
+++ interceptor/src/main/java/ar/com/servicioecu/interceptor/interceptor/aop/GreetingAspect.java
@@ -0,0 +1,12 @@
+package ar.com.servicioecu.interceptor.interceptor.aop;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Aspect
+public class GreetingAspect {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+}
