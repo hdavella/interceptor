3a9e6d5346a2e81acb014b6a8cd1dec86a7a07a1
HernanD <hdavella@gmail.com>
2025-08-29 18:39:55 -0300

    update

--- interceptor/src/main/java/ar/com/servicioecu/interceptor/interceptor/controller/InputController.java
+++ interceptor/src/main/java/ar/com/servicioecu/interceptor/interceptor/controller/InputController.java
@@ -4,14 +4,13 @@ import java.util.Collections;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import ar.com.servicioecu.interceptor.interceptor.service.Greeting;
 
-@EnableAspectJAutoProxy
+
 @RestController
 @RequestMapping("/app")
 public class InputController {
