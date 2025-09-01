72caf29f7f7a25e7dc35f3759ba221aa39bb15cd
HernanD <hdavella@gmail.com>
2025-08-31 22:03:34 -0300

    nuevo Getmapping

--- interceptor/src/main/java/ar/com/servicioecu/interceptor/interceptor/controller/InputController.java
+++ interceptor/src/main/java/ar/com/servicioecu/interceptor/interceptor/controller/InputController.java
@@ -4,8 +4,10 @@ import java.util.Collections;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import ar.com.servicioecu.interceptor.interceptor.service.Greeting;
@@ -36,5 +38,12 @@ public class InputController {
         System.out.println(s4);
         return Collections.singletonMap("message", "Hello World");
     }
+
+    @GetMapping("/greetingtoo")
+    public ResponseEntity<?> geetingToo(@RequestParam String param) {
+        
+        return ResponseEntity.ok(Collections.singletonMap("message", "Hello World Too"));
+    }
+    
  
 }
